2b0d3a00b1046826c429ab25628f7784ca584d47
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-01-02 14:54:06 +0100

    feat: Redirect to show details page and move show fetching logic to new dynamic route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,4 @@
-import { ShowCard, ShowEpisodes } from "@/components";
-
+import { redirect } from "next/navigation";
 export default async function Home() {
-  const response = await fetch("https://api.tvmaze.com/shows/6771", {
-    cache: "force-cache",
-  });
-  const data = await response.json();
-
-  return (
-    <div className="min-h-screen bg-neutral-50 dark:bg-neutral-950 py-8 px-4 md:py-12 md:px-6 space-y-8">
-      <ShowCard show={data} />
-      <ShowEpisodes showId={data.id} />
-    </div>
-  );
+  redirect("/shows/6771");
 }
